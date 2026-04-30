f7b430c260589854c81c6b9b20975d911f48915d
Pedro Santana <pedro.costa.santana@gmail.com>
2026-04-30 16:49:27 +0100

    fix black-box test for JetBrains homepage

--- src/test/java/iscteiul/ista/blackbattleship/MainPage.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPage.java
@@ -6,8 +6,15 @@ import static com.codeborne.selenide.Selenide.$x;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-  public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
-  public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
-  public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']");
+  // Botão de pesquisa no cabeçalho
   public SelenideElement searchButton = $("[data-test='site-header-search-action']");
+
+  // Menu "Products" na barra de navegação principal
+  public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker='Products']");
+
+  // Mesmo botão de menu "Products" usado para abrir o submenu e navegar para a página de ferramentas
+  public SelenideElement seeDeveloperToolsButton = $x("//div[@data-test='main-menu-item' and @data-test-marker='Products']");
+
+  // Link "Find your tool" que aparece no submenu de Products
+  public SelenideElement findYourToolsButton = $("[data-test='suggestion-link']");
 }
--- src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
@@ -13,38 +13,49 @@ import static com.codeborne.selenide.Selenide.*;
 public class MainPageTest {
     MainPage mainPage = new MainPage();
 
-@BeforeAll    public static void setUpAll() {
+    @BeforeAll
+    public static void setUpAll() {
         Configuration.browserSize = "1280x800";
+        Configuration.timeout = 10000;
+        Configuration.pageLoadTimeout = 60000;
+        Configuration.pageLoadStrategy = "eager";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
-@BeforeEach    public void setUp() {
+    @BeforeEach
+    public void setUp() {
         open("https://www.jetbrains.com/");
     }
 
     @Test
-    public void search() {
-        mainPage.searchButton.click();
+    public void search() throws InterruptedException {
+        mainPage.searchButton.shouldBe(visible).click();
+        Thread.sleep(1000);
 
-        $("[data-test='search-input']").sendKeys("Selenium");
+        $("[data-test-id='search-input']").shouldBe(visible).sendKeys("Selenium");
+        Thread.sleep(500);
         $("button[data-test='full-search-button']").click();
+        Thread.sleep(2000);
 
-        $("input[data-test='search-input']").shouldHave(attribute("value", "Selenium"));
+        $("[data-test-id='search-input']").shouldHave(attribute("value", "Selenium"));
     }
 
     @Test
-    public void toolsMenu() {
-        mainPage.toolsMenu.click();
+    public void toolsMenu() throws InterruptedException {
+        mainPage.toolsMenu.shouldBe(visible).click();
+        Thread.sleep(1000);
 
         $("div[data-test='main-submenu']").shouldBe(visible);
     }
 
     @Test
-    public void navigationToAllTools() {
-        mainPage.seeDeveloperToolsButton.click();
-        mainPage.findYourToolsButton.click();
+    public void navigationToAllTools() throws InterruptedException {
+        mainPage.seeDeveloperToolsButton.shouldBe(visible).click();
+        Thread.sleep(1000);
+        mainPage.findYourToolsButton.shouldBe(visible).click(); // suggestion-link sobrepõe suggestion-action
+        Thread.sleep(2000);
 
         $("#products-page").shouldBe(visible);
-
-assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());    }
+        assertEquals("All Developer Tools and Products by JetBrains", Selenide.title());
+    }
 }
